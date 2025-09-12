8ca9be1919b65ba9552a2d84601495184376e644
dheepaky <dhee3257@gmail.com>
2025-09-12 13:24:53 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use("/api/blog", blogrouter);
 app.use("/api/category", categoryrouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
   });
 }
 
