588358b2b1ae96e5236a3c3b773132bee6d39d18
dheepaky <dhee3257@gmail.com>
2025-09-12 10:48:46 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use("/api/category", categoryrouter);
 
 const PORT = process.env.PORT;
 
-if (process.env.NODE_ENV) {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
