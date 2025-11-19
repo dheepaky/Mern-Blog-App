1b9788c5b7244af861be5e53d9e14b533d427c1c
dheepaky <dhee3257@gmail.com>
2025-11-19 20:42:28 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -41,29 +41,17 @@ app.use("/api/auth", userRouter);
 
 app.use("/", sitemapRoute);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//   app.get(/.*/, (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
-//   });
-// }
-
-// app.get("/", (req, res) => {
-//   res.send("API is Running.........");
-// });
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running.........");
   });
 }
-
-app.get("/", (req, res) => {
-  res.send("API is Running.........");
-});
 
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
