a2d4635c3511e6cd6cdb15739c9f582d201e01ee
dheepaky <dhee3257@gmail.com>
2025-11-22 14:39:53 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -69,14 +69,14 @@ app.use("/", sitemapRoute);
 // }
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
-    res.send("API is Running.........");
+    res.send("API is Running...");
   });
 }
 
